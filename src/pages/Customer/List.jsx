af29b9941817c691e5ef8e5ee95c8dd920beb829
Sligliyange <gayathriishani997@gmail.com>
2025-02-18 17:53:45 +0530

    make all pages

---
+++ src/pages/Customer/List.jsx
@@ -0,0 +1,13 @@
+
+import CustomerList from "../../components/supplier/list/supplier";
+
+const List = () => {
+    return (
+        <>
+            <div>
+                <CustomerList />
+            </div>
+        </>
+    );
+};
+export default List;
